770db4974c85ae8cced1f26011c589a315f1c7bc
Yaolong Huang <airekans@gmail.com>
2014-03-16 22:38:19 +0800

    Create Child.h

---
+++ Child.h
@@ -0,0 +1,29 @@
+#ifndef _CHILD_H_
+#define _CHILD_H_
+
+#include "Base1.h"
+
+class Child : public Base
+{
+public:
+    Child(unsigned s, unsigned* d, int i)
+    {
+        seq = s;
+        data = d;
+        i_data = i;
+    }
+
+protected:
+    unsigned seq;
+    unsigned* data;
+    int i_data;
+};
+
+class SuperChild : public Child
+{
+public:
+    SuperChild();
+};
+
+
+#endif
